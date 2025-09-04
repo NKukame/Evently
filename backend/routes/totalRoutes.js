e111ac4af8eb73bb262637b22b6dc6b8733ea1f6
NKukame <NKukame@jhb.dvt.co.za>
2025-09-04 14:55:21 +0200

    Backend Set Up With Prisma Also Included.

--- backend/routes/totalRoutes.js
+++ backend/routes/totalRoutes.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import signUp from '../controllers/SignUpController';
 
 const totalRoutes = express.Router();
+
+totalRoutes.post("register", signUp);
 
 
 export default totalRoutes;
